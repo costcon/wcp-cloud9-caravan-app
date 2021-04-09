3d2f0502b18cadd8d69ee9e7cc14fa6206fabcf4
costcon <tentekomambo@gmail.com>
2021-04-09 07:44:11 +0000

    Finish 5-7.5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blog = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -13,6 +15,7 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
+
     blog.save
     redirect_to blogs_path(blog.id)
   end
